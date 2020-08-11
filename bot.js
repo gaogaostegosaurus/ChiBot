b1c849cfda2d64a6952128c601baa54d40a5fb97
unknown <gaogaostegosaurus@users.noreply.github.com>
2020-08-11 02:11:48 -0400

    Starting Google Sheets API work

--- bot.js
+++ bot.js
@@ -8,13 +8,127 @@
 // LOOK AT THESE FUCKING GLOBALS
 // MAN, ARE YOU IN THE MOOD FOR SOME FUCKING SPAGHETTI CODE TOO?
 
+// APIs and such
+const Discord = require('discord.js');
+
+// eslint-disable-next-line camelcase, import/order
+const { prefix, discord_token } = require('./discord.json');
+
+// Below copied from Google Sheets API Quickstart
+// https://developers.google.com/sheets/api/quickstart/nodejs
+// Changes are eslint exceptions and credentials json
+
+const fs = require('fs');
+const readline = require('readline');
+// eslint-disable-next-line object-curly-spacing
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+// eslint-disable-next-line consistent-return
+fs.readFile('sheets.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  // eslint-disable-next-line no-use-before-define
+  authorize(JSON.parse(content), listMajors);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  // eslint-disable-next-line object-curly-spacing, camelcase
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+  // eslint-disable-next-line indent
+      // eslint-disable-next-line function-paren-newline, indent
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  // eslint-disable-next-line consistent-return
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    // eslint-disable-next-line no-use-before-define
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    // eslint-disable-next-line consistent-return
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      // eslint-disable-next-line consistent-return, no-shadow
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+  // eslint-disable-next-line object-curly-spacing
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'Class Data!A2:E',
+  // eslint-disable-next-line consistent-return
+  }, (err, res) => {
+    // eslint-disable-next-line prefer-template
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log('Name, Major:');
+      // Print columns A and E, which correspond to indices 0 and 4.
+      // eslint-disable-next-line array-callback-return
+      rows.map((row) => {
+        console.log(`${row[0]}, ${row[4]}`);
+      });
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
+
 // Embed config variables
 const embedDelay = 1000; // ms to wait before changing embed (to prevent rate limiting?)
 const openString = '<OPEN>'; // Set string to designate open slots
 const maxJobIcons = 2; // Maximum number of icons next to a name - changes to role icon after this
-
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
@@ -606,4 +720,4 @@ client.on('message', (message) => {
   }
 });
 
-client.login(token);
+client.login(discord_token);
